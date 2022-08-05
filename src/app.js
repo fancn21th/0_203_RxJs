af9a993c38a0f14614a7b85cc5ba17ec2680c620
fancn21th <fancn21th@gmail.com>
2022-08-05 15:22:52 +0800

    first rx sample code

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+const button = document.querySelector("button");
+const label = document.querySelector("label");
+
+import { fromEvent } from "rxjs";
+
+fromEvent(button, "click").subscribe(() => {
+  label.innerHTML = "Clicked";
+});
+
+setInterval(() => {
+  const len = Math.floor(Math.random() * 3);
+  const arr = new Array(len).fill(".");
+  label.innerHTML = arr.join("");
+}, 1250);
