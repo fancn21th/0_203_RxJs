f5ab1ab6e9a170a44c1b592b7420d1119ca0815d
fancn21th <fancn21th@gmail.com>
2022-08-06 18:02:03 +0800

    ob as func

--- src/app.js
+++ src/app.js
@@ -1,16 +1,39 @@
 /**
- *  specify the dynamic behavior of a value completely at the time of its declaration
- *  在声明时完全指定一个值的动态行为
- *  行为编程
+ *  - RxJS is a library for composing asynchronous and event-based programs by using observable sequences.
+ *    It provides one core type, the Observable, satellite types (Observer, Schedulers, Subjects) and operators inspired by Array methods
+ *    (map, filter, reduce, every, etc) to allow handling asynchronous events as collections.
+ *  - specify the dynamic behavior of a value completely at the time of its declaration
+ *  - Think of RxJS as Lodash for events.
+ *  - push vs pull
+ *  - function + generator
+ *        pull single & multiple values
+ *      vs
+ *    promise + observable
+ *        push single & multiple values
+ *
  */
-const label = document.querySelector("label");
+import { Observable } from "rxjs";
 
-import { of, map } from "rxjs";
-
-const streamA = of(1, 2, 3);
-
-const streamB = streamA.pipe(map((x) => 10 * x));
-
-streamB.subscribe((x) => {
-  label.innerHTML += "," + x;
+const observable = new Observable((subscriber) => {
+  subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
+  setTimeout(() => {
+    subscriber.next(4);
+    subscriber.complete();
+  }, 1000);
 });
+
+console.log("just before subscribe");
+observable.subscribe({
+  next(x) {
+    console.log("got value " + x);
+  },
+  error(err) {
+    console.error("something wrong occurred: " + err);
+  },
+  complete() {
+    console.log("done");
+  },
+});
+console.log("just after subscribe");
